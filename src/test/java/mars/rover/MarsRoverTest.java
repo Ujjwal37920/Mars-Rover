cf156e6a327bfb225162f47d614eba895d57f879
Ujjwal Kumar <ujjwal.kumar@thoughtworks.com>
2022-05-03 21:55:14 +0530

    [Ujjwal] Add. Test for checking initial position is written

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -14,8 +14,28 @@ class MarsRoverTest {
     }
 
     @Test
-    public void
-    acceptance_test_2() {
+    void shouldReturnInitialPositionWhenNoCommandIsGiven() {
+        MarsRover marsRover=new MarsRover();
+        String actualPosition = "0 0 N";
+
+        String finalPosition = marsRover.move(0,0, 'N',"");
+
+        assertEquals(actualPosition, finalPosition);
+    }
+
+    @Test
+    void shouldFaceEastWhenRightInstructionIsGiven() {
+        MarsRover marsRover = new MarsRover();
+        String actualPosition = "0 0 E";
+
+        String finalPosition = marsRover.move(0,0,'N',"R");
+
+        assertEquals(actualPosition, finalPosition);
+    }
+
+
+    @Test
+    public void acceptance_test_2() {
         String newPosition = MarsRover.move(3, 3, 'E', "MMRMMRMRRM");
         assertEquals("5 1 E", newPosition);
     }
